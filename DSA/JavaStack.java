9c904229c15f16d73342d535905f872714593686
Pradyumna Hegde <117185220+Pradyumnahegde@users.noreply.github.com>
2023-02-11 20:06:43 +0530

    Add files via upload
    JavaStack file is Stack collection used in java.
    Stack file is array implementation of stacks.

---
+++ DSA/JavaStack.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+public class JavaStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack);
+        int p = stack.pop();
+        System.out.println(p);
+        System.out.println(stack);
+        stack.push(30);
+        stack.push(40);
+        int a = stack.peek();
+        System.out.println(a);
+        boolean check = stack.empty();
+        System.out.println(check);
+    }
+}
