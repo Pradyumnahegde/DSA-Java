c2109e1a2a5eb016c0f6a1287a328a0bcd572492
Pradyumna Hegde <117185220+Pradyumnahegde@users.noreply.github.com>
2023-02-11 20:07:32 +0530

    Update and rename JavaStack.java to StackCollection.java

--- DSA/StackCollection.java
+++ DSA/StackCollection.java
@@ -1,5 +1,5 @@
 import java.util.Stack;
-public class JavaStack {
+public class StackCollection {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(10);
